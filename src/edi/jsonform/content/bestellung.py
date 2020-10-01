f2b5b056522ee48d543efbeaa9e3391426811c92
seppowalther <seppo.walther@educorvi.de>
2020-10-01 10:37:50 +0200

    Implemented Iterator over fields and fieldnames

--- src/edi/jsonform/content/bestellung.py
+++ src/edi/jsonform/content/bestellung.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-# from plone.app.textfield import RichText
 # from plone.autoform import directives
 from plone.dexterity.content import Item
 # from plone.namedfile import field as namedfile
@@ -22,10 +20,19 @@ class IBestellung(model.Schema):
     vorname = schema.TextLine(title=_(u"Vorname"), required=True)
     nachname = schema.TextLine(title=_(u"Nachname"), required=True)
     artikelnummer = schema.Int(title=_(u"Artikelnummer"), required=True)
+    # country = schema.Choice(title = _(u"Country")
 
 @implementer(IBestellung)
 class Bestellung(Item):
+    schemadict={}
+    fields = schema.getFieldsInOrder(IBestellung)
+    for i in fields:
+        import pdb; pdb.set_trace()
+        schemadict[i[0]] = {}
+
+    import pdb; pdb.set_trace()
+    """
     def gen_json(self):
         fields = zope.schema.getFieldsInOrder(IBestellung)
         import pdb; pdb.set_trace()
-
+    """
